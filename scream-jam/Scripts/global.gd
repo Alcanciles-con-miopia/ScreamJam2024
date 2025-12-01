e6ed1954488e0ea5f780835c16cb33f1af5eafe5
Paigro <paigro9@gmail.com>
2025-12-01 23:49:27 +0100

    Compitruenos unidos.

--- scream-jam/Scripts/global.gd
+++ scream-jam/Scripts/global.gd
@@ -77,15 +77,17 @@ func generate_narrative() -> void:
 func _poner_los_creditos()->void:
 	totransition.emit(Scenes.CREDITS)
 
+# Para preparar el aleatorio de gente en sala.
 func _ready() -> void:
 	nPersonas = (randi() % 5) # Random entre 0 y 4 personas.
-	nextNPersonas = randfn(1.0, 20.0) # Tiempo para el siguente checkeo de nPersonas.
+	nextNPersonas = randfn(1.0, 10.0) # Tiempo para el siguente checkeo de nPersonas.
 	return
+# Para actualizar el aleatorio de gente en sala.
 func _process(_delta: float) -> void:
 	if nextNPersonas <= 0:
 		nPersonas = (randi() % 5) # Random entre 0 y 4 personas.
 		#AudioManager.set_ambience_param("Gente", nPersonas)
-		nextNPersonas = randfn(1.0, 20.0) # Tiempo para el siguente checkeo de nPersonas.	
+		nextNPersonas = randfn(1.0, 10.0) # Tiempo para el siguente checkeo de nPersonas.	
 	else:
 		nextNPersonas -= _delta
 	return
